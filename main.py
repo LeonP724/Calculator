e08ebddbb14820fa3231cddd5b19940724ed2d7e
Leon Pelc <leon.pelc@gmail.com>
2021-10-14 16:50:16 +0200

    Calculator

---
+++ main.py
@@ -0,0 +1,12 @@
+a = float(input("Enter 1. number: "))
+b = float(input("Enter 2. number: "))
+operation = input("Enter math operation, for example `+`, `-`, `*` or `/`: ")
+
+if operation == "+":
+    print(a + b)
+elif operation == "-":
+    print(a - b)
+elif operation == "*":
+    print(a * b)
+elif operation == "/":
+    print(a / b)
